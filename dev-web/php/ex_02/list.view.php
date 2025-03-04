10a974ba69cce99d3f384d3765f372b2f7d7791b
bigdanhe <bigdanhe@gmail.com>
2025-03-04 18:47:30 +0100

    ee

--- dev-web/php/ex_02/list.view.php
+++ dev-web/php/ex_02/list.view.php
@@ -54,96 +54,97 @@
     </nav>
     <div
         class="table-responsive w-100 mt-4 container p-5 bg-white shadow-sm">
-        <form class="w-full bg-white mb-2" action="" method="post">
-            <div class="row">
-                <div class="col-3">
-                    <div class="form-group">
-                        <input class="form-control" id="name" type="text" name="search_key" placeholder="Search">
+        <div class="w-100 d-flex justify-content-between align-items-center mb-3">
+            <div>Users</div>
+            <form class="w-100 bg-white" action="" method="post">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="form-group">
+                            <input class="form-control" id="name" type="text" name="search_key" placeholder="Search">
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-3  ">
-                    <div class="form-group">
-                        <input class="form-control" id="name" type="text" name="name_search_key" placeholder="Search by name">
+                    <div class="col-3  ">
+                        <div class="form-group">
+                            <input class="form-control" id="name" type="text" name="name_search_key" placeholder="Search by name">
+                        </div>
                     </div>
-                </div>
-                <div class="col-2">
-                    <input
-                        name="my-search-button"
-                        type="submit"
-                        value="Filtrer"
-                        id=""
-                        class="btn btn-primary w-100"
-                        href="#"
-                        role="button">
-                </div>
-                <div class="col-2">
-                    <input
-                        name="my-clearSearch-button"
-                        type="submit"
-                        value="Clear"
-                        id=""
-                        class="btn btn-danger w-100"
-                        href="#"
-                        role="button">
-                </div>
+                    <div class="col-2">
+                        <input
+                            name="my-search-button"
+                            type="submit"
+                            value="Filtrer"
+                            id=""
+                            class="btn btn-primary w-100"
+                            href="#"
+                            role="button">
+                    </div>
+                    <div class="col-2">
+                        <input
+                            name="my-clearSearch-button"
+                            type="submit"
+                            value="Clear"
+                            id=""
+                            class="btn btn-danger w-100"
+                            href="#"
+                            role="button">
+                    </div>
 
-            </div>
-        </form>
-        <div class="">
-            <span>Users</span>
-            <table
-                class="table   ">
-                <thead class="table-light">
+                </div>
+            </form>
+        </div>
+        <table
+            class="table   ">
+            <thead class="table-light">
+                <tr>
+                    <th scope="col"> #</th>
+                    <th scope="col"> Avatar</th>
+                    <th scope="col">Nom</th>
+                    <th scope="col">Prénom</th>
+                    <th scope="col">Diplôme</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (count($users) === 0): ?>
                     <tr>
-                        <th scope="col"> #</th>
-                        <th scope="col"> Avatar</th>
-                        <th scope="col">Nom</th>
-                        <th scope="col">Prénom</th>
-                        <th scope="col">Diplôme</th>
-                        <th scope="col">Age</th>
-                        <th scope="col">Action</th>
+                        <td colspan="6" class="text-center">
+                            Pas de données trouvées
+                        </td>
+
                     </tr>
-                </thead>
-                <tbody>
-                    <?php if (count($users) === 0): ?>
-                        <tr>
-                            <td colspan="6" class="text-center">
-                                Pas de données trouvées
+
+                <?php else: ?>
+                    <?php foreach ($users as $key => $user):
+                    ?>
+
+                        <tr class="">
+                            <td scope="row"><?= $user['id'] ?></td>
+                            <td scope="row"><img src="<?= substr($user['profile_image_url'], 1)  ?>" alt="Image" width="70" height="70" style="object-fit:cover"></td>
+                            <td scope="row"><?= $user['last_name'] ?></td>
+                            <td scope="row"><?= $user['first_name'] ?></td>
+                            <td scope="row"><?= $user['degree'] ?></td>
+                            <td scope="row"><?= $user['age'] ?></td>
+                            <td scope="row">
+                                <div>
+                                    <a class="btn btn-primary" href="./detail.php?id=<?= $user['id'] ?>">
+                                        Details
+                                    </a>
+                                    <a class="btn btn-warning" href="./edit.php?id=<?= $user['id'] ?>&first_name=<?= $user['first_name'] ?>&last_name=<?= $user['last_name'] ?>&degree=<?= $user['degree'] ?>&age=<?= $user['age'] ?>&description=<?= $user['description'] ?>&profile_image_url=<?= $user['profile_image_url'] ?>">
+                                        Modifier
+                                    </a>
+
+                                </div>
                             </td>
 
                         </tr>
+                    <?php endforeach ?>
+                <?php endif ?>
 
-                    <?php else: ?>
-                        <?php foreach ($users as $key => $user):
-                        ?>
+            </tbody>
+        </table>
 
-                            <tr class="">
-                                <td scope="row"><?= $user['id'] ?></td>
-                                <td scope="row"><img src="<?= substr($user['profile_image_url'], 1)  ?>" alt="Image" width="70" height="70" style="object-fit:cover"></td>
-                                <td scope="row"><?= $user['last_name'] ?></td>
-                                <td scope="row"><?= $user['first_name'] ?></td>
-                                <td scope="row"><?= $user['degree'] ?></td>
-                                <td scope="row"><?= $user['age'] ?></td>
-                                <td scope="row">
-                                    <div>
-                                        <a class="btn btn-primary" href="./detail.php?id=<?= $user['id'] ?>">
-                                            Details
-                                        </a>
-                                        <a class="btn btn-warning" href="./edit.php?id=<?= $user['id'] ?>&first_name=<?= $user['first_name'] ?>&last_name=<?= $user['last_name'] ?>&degree=<?= $user['degree'] ?>&age=<?= $user['age'] ?>&description=<?= $user['description'] ?>&profile_image_url=<?= $user['profile_image_url'] ?>">
-                                            Modifier
-                                        </a>
-
-                                    </div>
-                                </td>
-
-                            </tr>
-                        <?php endforeach ?>
-                    <?php endif ?>
-
-                </tbody>
-            </table>
-        </div>
 
     </div>
 
