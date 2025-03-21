e94352b0eb6b76ab74ba89798198b8e0db6a410a
bigdanhe <bigdanhe@gmail.com>
2025-03-21 19:00:01 +0100

    update

--- dev-web/proto/app/pages/livres/index.page.php
+++ dev-web/proto/app/pages/livres/index.page.php
@@ -1,9 +1,9 @@
 <?php page('partials/head.php') ?>
 <main class="d-flex">
     <?php page('partials/sidebar.php') ?>
-    <section class="bg-light w-100">
+    <section class="bg-light w-100" style="height: 100dvh;">
         <?php page('partials/navbar.php', ['header' => 'Banner']) ?>
-        <div class="p-3">
+        <div class="p-3 bg-white m-3 shadow-sm" style="height: calc(100dvh-70px);">
             Bonjour
         </div>
     </section>
--- dev-web/proto/app/pages/partials/navbar.php
+++ dev-web/proto/app/pages/partials/navbar.php
@@ -1,3 +1,5 @@
-<div class="w-100 bg-light shadow-sm" style="height: 50px;">
-    Nav
+<div class="w-100 bg-secondary text-white p shadow-sm ps-3 fs-4 d-flex align-items-center" style="height: 70px;">
+    <span>
+        Livres
+    </span>
 </div>
